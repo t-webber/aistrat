e45f3ba7339c99c24600646fb4e190042d1aa5bc
Nicolas Besson <nicolas.besson@telecom-paris.fr>
2024-02-28 14:09:37 +0100

    Co-authored-by: Tom Webber <t-webber@users.noreply.github.com>

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import requests
-ip="http://137.194.145.251:8080/"
+ip="http://localhost:8080"
 player=""
 token=""
 
@@ -13,7 +13,35 @@ def getplayer():
         token=L['token']
     return(player,token)
 
+def movePawn(player,kind,oldy,oldx,newy,newx,token) -> bool:
+    try: requests.post(ip+"/move/"+player+"/"+kind+"/"+oldy+"/"+oldx+"/"+newy+"/"+newx+"/"+token,timeout=1)
+    except: return(False)
+    return(True)
+    
+def buildKind(player, kind, y, x, token) -> bool:
+    try : print(requests.post(f"{ip}/build/{player}/{y}/{x}/{kind}/{token}", timeout=1).json()) ; return True
+    except: return False
+
+def getMap(player,token):
+    try: R=requests.get(ip+"/view/"+str(player)+'/'+str(token)).json()
+    except: return None
+    return R
+
+def farm(player, y, x, token) -> bool:
+    try : print(requests.post(f"{ip}/farm/{player}/{y}/{x}/{token}", timeout=1).json()) ; return True
+    except: return False
+
+def build(player,y,x,kind,token) -> bool:
+    try: requests.post("/build/"+player+"/"+y+"/"+x+"/"+kind+"/"+token,timeout=1)
+    except: return False
+    return True
+    
+def autofarm(player, token) -> bool:
+    try : print(requests.post(f"{ip}/autofarm/{player}/{token}", timeout=1).json()) ; return True
+    except: return False
+
 getplayer()
-R=requests.get(ip+"/view/"+str(player)+'/'+str(token)).json()
+R=getMap(player,token)
 print(R)
 print(R['map'])
+
