18fadd0042255bbcdbe8c4779a0d525df21114d8
Nicolas Besson <nicolas.besson@telecom-paris.fr>
2024-02-28 14:21:10 +0100

    Co-authored-by: Tom Webber <t-webber@users.noreply.github.com>

--- api.py
+++ api.py
@@ -6,39 +6,44 @@ token=""
 def getplayer():
     global player
     global token
-    if player=="":
-        L=requests.get(ip+"/getToken",timeout=2.5).json()
-        print(L)
-        player=L['player']
-        token=L['token']
+    if ""==player:
+        dataplayer=requests.get(ip+"/getToken",timeout=2.5).json()
+        print(dataplayer)
+        player=dataplayer['player']
+        token=dataplayer['token']
     return(player,token)
 
-def movePawn(player,kind,oldy,oldx,newy,newx,token) -> bool:
+def movePawn(kind,oldy,oldx,newy,newx) -> bool:
     try: requests.post(ip+"/move/"+player+"/"+kind+"/"+oldy+"/"+oldx+"/"+newy+"/"+newx+"/"+token,timeout=1)
     except: return(False)
     return(True)
     
-def buildKind(player, kind, y, x, token) -> bool:
+def buildKind(kind, y, x) -> bool:
     try : print(requests.post(f"{ip}/build/{player}/{y}/{x}/{kind}/{token}", timeout=1).json()) ; return True
     except: return False
 
-def getMap(player,token):
-    try: R=requests.get(ip+"/view/"+str(player)+'/'+str(token)).json()
+def getMap():
+    try: carte=requests.get(ip+"/view/"+str(player)+'/'+str(token)).json()
     except: return None
-    return R
+    return carte
 
-def farm(player, y, x, token) -> bool:
+def farm(y, x) -> bool:
     try : print(requests.post(f"{ip}/farm/{player}/{y}/{x}/{token}", timeout=1).json()) ; return True
     except: return False
 
-def build(player,y,x,kind,token) -> bool:
+def build(y,x,kind) -> bool:
     try: requests.post("/build/"+player+"/"+y+"/"+x+"/"+kind+"/"+token,timeout=1)
     except: return False
     return True
     
-def autofarm(player, token) -> bool:
+def autofarm() -> bool:
     try : print(requests.post(f"{ip}/autofarm/{player}/{token}", timeout=1).json()) ; return True
     except: return False
+
+def getinfo(y,x):
+    return getMap(player,token)[y][x]
+
+
 
 getplayer()
 R=getMap(player,token)
