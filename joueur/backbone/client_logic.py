96fab3f428a3d9c5b7f032fb011c8590116d94c1
Tom Webber <t.c.w.webber@gmail.com>
2024-03-13 10:20:55 +0100

    Moving prediction combat

--- joueur/backbone/client_logic.py
+++ joueur/backbone/client_logic.py
@@ -1,9 +1,22 @@
 from scipy.optimize import linear_sum_assignment
 import numpy as np
 
-def hongroisDistance(acteurs,objets):
-    matrice=np.abs(np.array(acteurs)[:, np.newaxis] - np.array(objets))
+
+def hongroisDistance(acteurs, objets):
+    matrice = np.abs(np.array(acteurs)[:, np.newaxis] - np.array(objets))
     return algoHongrois(matrice)
+
 
 def algoHongrois(matriceHongrois):
     return zip(linear_sum_assignment(matriceHongrois))
+
+
+def prediction_combat(a, d):  # prédit le vainqueur d'un combat
+    pertes_a = 0
+    pertes_b = 0
+    while a > 0 and d > 0:
+        pertes_a += (d + 1)//2
+        pertes_b += (a + 1)//2
+        a = a - (d + 1)//2
+        d = d - (a + 1)//2
+    return (d <= 0, pertes_a, pertes_b)  # true si l'attaque gagne
