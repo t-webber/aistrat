bf404609a587979be412958fd037c115a673e88d
Nicolas Besson <nicolas.besson@telecom-paris.fr>
2024-03-13 11:23:39 +0100

    Restauration de cleanGolds

--- joueur/backbone/client_logic.py
+++ joueur/backbone/client_logic.py
@@ -3,15 +3,21 @@
 from scipy.optimize import linear_sum_assignment
 import numpy as np
 
-def distance(x1, y1, x2, y2):
-    """ 
-    distance between coordinates
-    """
-    return abs(x1-x2)+abs(y1-y2)
-
 def hongroisDistance(acteurs,objets):
     matriceCost= np.abs(np.array(acteurs)[:, np.newaxis] - np.array(objets)).sum(axis=2)
     return algoHongrois(matriceCost)
+
+def cleanGolds(golds): #Priorise les petits tas à côté des gros tas et n'y envoie qu'un seul péon
+    toBeRemoved=[0 for _ in range(len(golds))]
+    for num in range(len(golds)):
+        pile=golds[num]
+        if pile[2]<16 and toBeRemoved[num]==0:
+            for i in range(len(golds)):
+                if i!=num and distance(pile[0],pile[1],golds[i][0],golds[i][1])<=1:
+                    toBeRemoved[i]=1
+    goldClean=[golds[i] for i in range(len(golds)) if toBeRemoved[i]==0]
+    print(golds,goldClean,toBeRemoved)
+    return goldClean
 
 def algoHongrois(matriceHongrois):
     a,b=linear_sum_assignment(matriceHongrois)
