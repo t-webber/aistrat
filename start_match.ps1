a57e8476c27d76830edf176cadd8d0a3cf6bfd35
Tom Webber <t.c.w.webber@gmail.com>
2024-04-09 09:40:37 +0200

    add 2 players in cli start_match

--- start_match.ps1
+++ start_match.ps1
@@ -1,1 +1,11 @@
-python "$PSScriptRoot/1player.py" $args[1]
+param(
+    [string]$url, 
+    [Alias("two")]
+    [switch]$twoplayers
+)
+
+if ($twoplayers) {
+    python "$PSScriptRoot/2players.py" $url
+} else {
+    python "$PSScriptRoot/1player.py" $url
+}
