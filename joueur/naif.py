e2507685f87e9a8dd21bef4e2ea6c339453638ea
Jules Fraizier <jules.fraizier@telecom-paris.fr>
2024-04-09 09:21:14 +0200

    correction fuite

--- joueur/naif.py
+++ joueur/naif.py
@@ -25,7 +25,7 @@ def fuite(pawns, knights, eknights, defense, player, token):
             if cl.prediction_combat(total_enemies, allies+allies_backup)[0]:
                 # si on peut perd le combat même avec les alliés on fuit
                 for direc in direc_enemies:
-                    if direc_enemies[direc] == 0:
+                    if direc_enemies[direc] == 0 and (p[0] + direc[0], p[1]+direc[1]) in api.get_moves(p[0], p[1]):
                         api.move(api.PAWN, p[0], p[1], p[0] +
                                 direc[0], p[1]+direc[1], player, token)
                         pawns.remove((p[0], p[1]))
@@ -351,7 +351,6 @@ def nexturn(player, token):
             defense.remove(d)
         else:
             knights.remove(d)
-
     good_gold, bad_gold = cl.clean_golds(golds, pawns)
 
     left_defense=defend(pawns, defense, eknights, castles, player, token)
