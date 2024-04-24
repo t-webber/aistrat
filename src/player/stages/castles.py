c78ed48de0f317c8945715e83d26e51b06673c48
soupierre <pierreneltner@gmail.com>
2024-04-24 14:53:12 +0200

    nettoyage de client logic et uniformisation de la langue des commentaires de castles + création de chevaliers plus tôt et plus equilibrée

--- src/player/stages/castles.py
+++ src/player/stages/castles.py
@@ -16,53 +16,53 @@ def check_build(pawns, castles, player, token, gold):
     """ Build some castle, and, at the beggining, take control of a pawn to build the first castle in the right place  """
     len_y, len_x = api.size_map()
 
-    # define borders to not build castles on the borders
+    # définis les bordures pour ne pas y construire de château
     border = 2
     border_x = len_x - 1 - border
     border_y = len_y - 1 - border
 
-    # adapt side to the player : player A (up left) or player B (down right)
+    # adapte le côté au joueur : joueur A (haut gauche) ou joueur B (bas droite)
     good = (border, border) if player == "A" else (
         border_y, border_x)
 
-    # if no castles where built, take control of a pawn and put him in (2, 2), then build a castle
+    # Si aucun château n'a été construit, prend le controle d'un pion, le met en (2,2) et ensuite construit un château
     if len(castles) == 0:
         d = distance_2_castle(good[0], good[1], pawns)
-        if d != 0:  # if d == 0, the pawn is in the right place so it will build a castle bellow
+        if d != 0:  # si d == 0, le pion est au bon endroit donc il va construire un château ici
             for (y, x) in pawns:
 
-                # this pawn is the nearest from the good place
+                # ce pions est le plus proche de la bonne localisation
                 if cl.distance(y, x, good[0], good[1]) == d:
                     if player == "A":
 
-                        # near enough from the destination in x : move y
+                        # assez proche de la destination en x : déplacement en y
                         if x >= border:
                             api.move(api.PAWN, y, x, y +
                                      1, x, player, token)
-                        else:  # move x
+                        else:  # déplacement en x
                             api.move(api.PAWN, y, x, y,
                                      x+1, player, token)
                     else:
-                        # near enough from the destination in x : move y
+                        # assez proche de la destination en x : déplacement en y
                         if x <= border_x:
                             api.move(api.PAWN, y, x, y -
                                      1, x, player, token)
-                        else:  # move x
+                        else:  # déplace en x
                             api.move(api.PAWN, y, x, y,
                                      x-1, player, token)
                     pawns.remove((y, x))
                     break
 
-    # if there are enough castles or not enough gold, don't try anything
+    # Si il y a suffisemment de châteaux ou pas assez d'argent, n'essaie rien 
     if len(castles) >= min(len_y, len_x) // 2 or gold < api.PRICES[api.CASTLE]:
         return
 
-    # build a castle at the first time the pawn is far enough from the existing castles
+    # construit un château au premier moment où un pion est suffisemment loin des châteaux existants
     for pawn in pawns:
         y, x = pawn
-        d = distance_2_castle(y, x, castles)  # distance to the nearest castle
+        d = distance_2_castle(y, x, castles)  # distance au château le plus proche
 
-        # if the pawn is far enough from the bortders
+        # si le pions est suffisamment loin de la bordure
         if border <= x <= border_x and border <= y <= border_y and d >= 3:
             api.build(api.CASTLE, y, x, player, token)
             pawns.remove(pawn)
@@ -75,21 +75,26 @@ def create_pawns(castles, player, token, eknight, knight, gold, defenders, nb_go
     n = len(eknight) - len(knight)
 
     for (y, x) in castles:
-        # We are under the attack ! Create some defenders !
+        # Nous somme attaqués, production de défenseurs 
         if n > 0:
-            if gold > api.PRICES[api.KNIGHT]*2:
+            if gold > api.PRICES[api.KNIGHT]:
                 api.build(api.KNIGHT, y, x, player, token)
                 gold -= api.PRICES[api.KNIGHT]
                 defenders.append((y, x))
                 api.PRICES[api.KNIGHT] -= 1
                 n -= 1
 
-        # Too much gold, build some attackers
+        # garder un equilibre entre defense et attaque et produire plus tôt
+        elif gold> api.PRICES[api.KNIGHT] and (len(knight)<=(1/2*len(defenders)) or len(knight)<=2/3*len(defenders)):
+            if api.build(api.KNIGHT, y, x, player, token):
+                gold -= api.PRICES[api.KNIGHT]
+
+        # trop d'argent on achète des défenseurs
         elif gold > api.PRICES[api.KNIGHT] * 2 and len(castles) >= 2:
             if api.build(api.KNIGHT, y, x, player, token):
                 gold -= api.PRICES[api.KNIGHT]
 
-        # Missing gold, and there is avaible gold on the map (or fog: places we can't see)
+        # Pas assez d'argent, et de l'argent est disponible sur la carte (ou du brouillard de guerre)
         elif gold > api.PRICES[api.PAWN] * 1.25 and nb_gold + nb_fog > nb_pawn:
             api.build(api.PAWN, y, x, player, token)
             gold -= api.PRICES[api.PAWN]
