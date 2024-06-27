681012ac92ee666f27bfe8626a8876fca18451f5
Tom Webber <t.c.w.webber@gmail.com>
2024-06-27 10:50:23 +0200

    add cli with all strategies matches !

--- start_match.ps1
+++ start_match.ps1
@@ -1,12 +1,27 @@
 param(
     [string]$url, 
+    [int]$port,
+    [string]$strategy1 = "glouton",
+    [string]$strategy2 = "memory",
     [Alias("two")]
     [switch]$twoplayers
 )
 
 clear-host
+
+if (!$port) {
+    $port = 8080
+}
+
+if (!$url) {
+    $url = "http://localhost:$port"   
+}
+
+Start-Process PowerShell -ArgumentList "-NoExit", "-Command", "python $PSscriptRoot/game/server.py $port"
+
 if ($twoplayers) {
-    python "$PSScriptRoot/src/main.py" 2 $url
+    Start-Process PowerShell -ArgumentList "-NoExit", "-Command", "python $PSScriptRoot/strategies/$strategy1/main.py 1 $url"
+    Start-Process PowerShell -ArgumentList "-NoExit", "-Command", "python $PSScriptRoot/strategies/$strategy2/main.py 1 $url"
 } else {
-    python "$PSScriptRoot/src/main.py" 1 $url
+    Start-Process PowerShell -ArgumentList "-NoExit", "-Command", "python $PSScriptRoot/strategies/$strategy1/main.py 1 $url"
 }
